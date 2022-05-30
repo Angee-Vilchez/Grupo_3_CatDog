bbeddc2d60daa2f7b38e22a1284a8cbcc98e9218
Daniel Mamani <danielmamani18.07@gmail.com>
2022-05-30 16:56:39 -0300

    falla en usersession, por ahora sera borrado

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -7,17 +7,17 @@ const uploadFile = require('../middlewares/uploadProduct');
 const userSession = require('../middlewares/userSession');
 
 /* Session */
-router.get('/', userSession, adminCheck, adminController.index);
+router.get('/', adminController.index);
 
 /* CRUD DE PRODUCTOS */
 /* GET - LISTADO DE PRODUCTOS */
-router.get('/productos', userSession, adminCheck, adminProductsController.list);
+router.get('/productos',  adminProductsController.list);
 /* GET - CREACION DE PRODUCTO */
-router.get('/productos/agregar', userSession, adminCheck, adminProductsController.productAdd);
+router.get('/productos/agregar', adminProductsController.productAdd);
 /* POST - CREAR PRODUCTO */
 router.post('/productos',uploadFile.single('image'), adminProductsController.productCreate);
 /* GET - EDITAR PRODUCTO */
-router.get('/productos/editar/:id', userSession, adminCheck, adminProductsController.productEdit);
+router.get('/productos/editar/:id',  adminProductsController.productEdit);
 /* PUT - ACTUALIZAR PRODUCTO */
 router.put('/productos/:id',uploadFile.single('image'), adminProductsController.productUpdate);
 /* DELETE - ELIMIAR UN PRODUCTO */
